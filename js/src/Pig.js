b397ccfd0c21622b4e63971aa7de00b3e6b6ad89
Tor Einar Lyngset <lyngset@gmail.com>
2014-03-13 22:55:22 +0100

    Added animations to pig

--- js/src/Pig.js
+++ js/src/Pig.js
@@ -16,14 +16,15 @@ Pig = function(game, x, y, scale) {
     this.scale.x = this.scale.y = this.scaler;
 
     this.anims = [
-                    {name: 'stand',         sequence: [],  frameRate: 1,   loop: true},
-                    {name: 'walk',          sequence: [],  frameRate: 1,   loop: true},
-                    {name: 'run',           sequence: [],  frameRate: 10,  loop: true},
-                    {name: 'wriggleHead',   sequence: [],  frameRate: 10,  loop: true},
-                    {name: 'straightTail',  sequence: [],  frameRate: 10,  loop: true},
-                    {name: 'jump',          sequence: [],  frameRate: 10,  loop: true},
-                    {name: 'limp',          sequence: [],  frameRate: 10,  loop: true},
-                    {name: 'sit',           sequence: [],  frameRate: 10,  loop: true}
+                    {name: 'stand',         sequence: [0],  frameRate: 1,   loop: false},
+                    {name: 'walk',          sequence: [0,5,6,5,0,7,8,7],  frameRate: 20,   loop: true},
+                    {name: 'run',           sequence: [0,5,6,5,0,7,8,7,0],  frameRate: 60,  loop: true},
+                    {name: 'wriggleHead',   sequence: [0,23,24,23,0,25,26,25,23,24,23,0,25,26,25,0],  frameRate: 10,  loop: false},
+                    {name: 'straightTail',  sequence: [0,19,20,21,22,22,22,21,20,19,0],  frameRate: 10,  loop: false},
+                    {name: 'jump',          sequence: [0,9,10,11,12,13,13,13,13,13,13,13,13,13,12,11,10,9,0],  frameRate: 20,  loop: false},
+                    {name: 'limp',          sequence: [0,1,2,1,0,3,4,3],  frameRate: 20,  loop: true},
+                    {name: 'sit',           sequence: [0,14,15,16,17,18],  frameRate: 20,  loop: false},
+                    {name: 'raise',         sequence: [18,17,16,15,14,0],  frameRate: 20,  loop: false}
 
     ];
 
@@ -31,7 +32,7 @@ Pig = function(game, x, y, scale) {
         that.animations.add(anim.name, anim.sequence, anim.frameRate, anim.loop, true);
     });
 
-    this.animations.play('drive');
+    this.animations.play('walk');
 
     // Enable input actions
     this.inputEnabled = true;
